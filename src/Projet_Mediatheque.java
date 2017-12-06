35f59ed320a57bc769fd35c590bb974e674ec05f
JBrignon <brignon.julien@gmail.com>
2017-12-06 15:40:34 +0100

    Ajout substantiels

--- src/Projet_Mediatheque.java
+++ src/Projet_Mediatheque.java
@@ -4,16 +4,23 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class Projet_Mediatheque {
-
-    
-    public static void main(String[] args) throws IOException {
-        Mediatheque Med = new Mediatheque();
-        Adherent Ad = new Adherent("pNom", "pPrenom", LocalDate.now(), "pMail", "pMDP", 01, 06);
-        Auteur Aut = new Auteur("pNom", "pPrenom", LocalDate.now(), 01, "pStyle");
-        Roman Rom = new Roman(01, "pTitre" , Aut, "pEditeur", 01, "pGenre", true);
-        Emprunt Emp = new Emprunt(Ad, Rom);
-        System.out.println(Emp.toString());
-    }
-    
+public class Projet_Mediatheque 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		Mediatheque Med = new Mediatheque();
+		menu();
+		
+	}
+	public static void menu()
+	{
+		clear();
+		System.out.println("BONJOUR JE SUIS UNE MEDIATHEQUE");
+	}
+	//Methode qui nettoie l'affichage
+	public static void clear()
+	{
+		System.out.print("\u001b[2J" + "\u001b[H");
+		System.out.flush();
+	}
 }
